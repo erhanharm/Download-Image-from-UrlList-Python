c67d50473dc45c3d3861cadc8e2944152fffcaae
ERHAN HARMANKAYA <erhanharm@gmail.com>
2017-04-23 11:54:06 +0300

    Line ending char issue fix

--- download.py
+++ download.py
@@ -8,7 +8,6 @@ pathname = os.path.dirname(sys.argv[0])#get current script path
 
 with open("file.txt", "r") as ins:  #read file for imagelinks
     for line in ins: # each line is a URL --sample URL https://venderfirm.theircdn.com/productimages/i/b2342arco34234de/[img][5][1].jpg
-    	print line
         urlsplit = line.split("/") 
         #print distutils.dir_util.mkpath(urlsplit[5]) #create a path for nonexisting barcode
         #print os.path.abspath(pathname)
@@ -16,12 +15,15 @@ with open("file.txt", "r") as ins:  #read file for imagelinks
         #print destPath
         #urllib.urlretrieve(line, os.path.abspath(pathname)+urlsplit[5]+"deneme.jpg")
         try:
-    		page = requests.get(line)
-    		print page.status_code
-    		if page.status_code != 200:
+            print line
+            page = requests.get(line.replace("\r","").replace("\n","")) # line ending chars throws 404 issue 
+            #page = requests.get('https://img-morhipo.mncdn.com/productimages/i/MH114006038318/[img][5][1].jpg')
+            print page.status_code
+            if page.status_code != 200:
     			print 'url not found'
-    		else:
-    			print distutils.dir_util.mkpath(urlsplit[5])
-    			urllib.urlretrieve(line,destPath)
+            else:
+                print 'URL FOUND AND DIRECTORY CREATED'
+                print distutils.dir_util.mkpath(urlsplit[5])
+                urllib.urlretrieve(line,destPath)
         except Exception,e:
     		print e
